24c6751a9ec1a7b46d868e6d62fe4e59e3fc1f45
JokubasPukinskas <jokubas.pukinskas@stud.kitm.lt>
2025-05-16 00:32:37 +0300

    Delete 25-05-15 directory

--- 25-05-15/surprise.js
+++
@@ -1,6 +0,0 @@
-let surpriseBtn = document.getElementById("surprise")
-
-surpriseBtn.addEventListener("click", function(){
-    let random = Math.floor(Math.random() * 731);
-    localStorage.setItem("searching", random);
-})
